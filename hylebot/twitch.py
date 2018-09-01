3e5c4e3eadc5689bf3c4d4692018115222e61406
cduongt <cduongt@gmail.com>
2018-09-01 03:47:23 +0200

    Add basic command options

--- hylebot/twitch.py
+++ hylebot/twitch.py
@@ -1,13 +1,15 @@
 import irc.bot
+import irc.strings
 import hylebot.osu
 import hylebot.message
 
 class TwitchBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, host, port, nickname, channel, token, database):
+    def __init__(self, host, port, nickname, channel, token, database, mods):
         irc.bot.SingleServerIRCBot.__init__(self, [(host, port, token)], nickname, nickname)
         self.channel = channel
         self.nickname = nickname
         self.db = database
+        self.mods = mods
 
     def on_welcome(self, connection, event):
         print(event)
@@ -21,5 +23,26 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
     
     def on_pubmsg(self, connection, event):
         print(event)
+        if (irc.strings.lower(event.source.nick) in self.mods):
+            self.do_command(event)
+
+    def do_command(self, event):
+        connection = self.connection
+        message = event.arguments[0].split(" ", 2)
+        command = message[0]
+        
+        if command == "!add" and len(message) > 2:
+            if self.db.get(message[1]):
+                connection.privmsg(self.channel, "Command " + message[1] + " is updated.")
+            else:
+                connection.privmsg(self.channel, "Command " + message[1] + " iss added.")
+            self.db.set(message[1], message[2])
+        elif command == "!delete" and len(message) > 1:
+            connection.privmsg(self.channel, "Command " + message[1] + " is deleted.")
+            self.db.delete(message[1])
+        elif command.startswith("!"):
+            if self.db.get(command):
+                connection.privmsg(self.channel, self.db.get(command))
+
                 
         
