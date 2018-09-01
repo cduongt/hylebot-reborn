6f609e31a64c0dafbf08af979276993de92f6a2b
cduongt <cduongt@gmail.com>
2018-09-01 03:46:54 +0200

    Add remove to database

--- hylebot/database.py
+++ hylebot/database.py
@@ -2,10 +2,13 @@ import redis
 
 class Database():
     def __init__(self, host, port, db):
-        self._database = redis.StrictRedis(host=host, port=port, db=db)
+        self._database = redis.StrictRedis(host=host, port=port, db=db, decode_responses=True)
     
     def set(self, key, value):
         self._database.set(key, value)
 
     def get(self, key):
         return self._database.get(key)
+
+    def delete(self, key):
+        self._database.delete(key)
